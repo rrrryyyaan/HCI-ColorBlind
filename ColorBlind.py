b57aafcdb09ea9ef9a763d7eae74299f8df0dde0
rrrryyyaan <48599743+rrrryyyaan@users.noreply.github.com>
2021-02-04 02:57:38 -0500

    Added Timing + Functionality

--- ColorBlind.py
+++ ColorBlind.py
@@ -1,6 +1,18 @@
 import pygame, pygame.freetype, sys, time, random
 from pygame.locals import *
 
+# Color Blind Bus Stop Interface
+# Project 1
+# @Authors Ryan, Sam
+# Date Created: 2/3/2021
+
+# currentColor values
+# 0 = red, 1 = brown, 2 = green, 3 = yellow, 4 = blue, 5 = purple
+
+pygame.init()
+size = (width, height) = (685, 400)
+DISPLAY = pygame.display.set_mode(size)
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (234, 153, 153)
@@ -12,109 +24,188 @@ PURPLE = (53, 28, 117)
 
 MAX_AMOUNT = 15
 
-def button(display, x, y, w, h, color, action=None):
+def button(x, y, w, h, color, action=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     if x + w > mouse[0] > x and y + h > mouse[1] > y:
-        pygame.draw.rect(display, BLACK, (x, y, w, h), 2, 2, 2, 2, 2)
+        pygame.draw.rect(DISPLAY, BLACK, (x, y, w, h), 2, 2, 2, 2, 2)
 
         if click[0] == 1 and action != None:
             action()
     else:
-        pygame.draw.rect(display, color, (x, y, w, h))
+        pygame.draw.rect(DISPLAY, color, (x, y, w, h))
 
-def text(display, msg, x, y):
+def text(msg, x, y):
     font = pygame.freetype.SysFont('arial', 20)
-    font.render_to(display, (x // 2, y // 2), msg, BLACK)
+    font.render_to(DISPLAY, (x // 2, y // 2), msg, BLACK)
     pygame.display.flip()
 
+def erase(x, y, w, h):
+    pygame.draw.rect(DISPLAY, WHITE, (x, y, w, h))
+
 def selectRed():
-    print("red")
+    if currentColor == 0:
+        print("Correct! Red")
+        displayActual(False)
+    else:
+        print("Wrong color..")
 
 def selectBlue():
-    print("blue")
+    if currentColor == 4:
+        print("Correct! Blue")
+        displayActual(False)
+    else:
+        print("Wrong color..")
 
 def selectGreen():
-    print("green")
+    if currentColor == 2:
+        print("Correct! Green")
+        displayActual(False)
+    else:
+        print("Wrong color..")
 
 def selectPurple():
-    print("purple")
+    if currentColor == 5:
+        print("Correct! Purple")
+        displayActual(False)
+    else:
+        print("Wrong color..")
 
 def selectYellow():
-    print("yellow")
+    if currentColor == 3:
+        print("Correct! Yellow")
+        displayActual(False)
+    else:
+        print("Wrong color..")
 
 def selectBrown():
-    print("brown")
+    if currentColor == 1:
+        print("Correct! Brown")
+        displayActual(False)
+    else:
+        print("Wrong color..")
 
-def selectCurrent():
-    print("current")
-
-def showCurrent(DISPLAY):
+def displayCurrent():
+    global currentColor
     randomNumber = random.randint(0, 5)
+    while(currentColor == randomNumber):
+        randomNumber = random.randint(0, 5)
     if randomNumber == 0:
-        button(DISPLAY, 540, 230, 100, 100, RED, selectCurrent)
+        pygame.draw.rect(DISPLAY, RED, (540, 230, 100, 100))
     elif randomNumber == 1:
-        button(DISPLAY, 540, 230, 100, 100, BLUE, selectCurrent)
+        pygame.draw.rect(DISPLAY, BROWN, (540, 230, 100, 100))
     elif randomNumber == 2:
-        button(DISPLAY, 540, 230, 100, 100, GREEN, selectCurrent)
+        pygame.draw.rect(DISPLAY, GREEN, (540, 230, 100, 100))
     elif randomNumber == 3:
-        button(DISPLAY, 540, 230, 100, 100, BROWN, selectCurrent)
+        pygame.draw.rect(DISPLAY, YELLOW, (540, 230, 100, 100))
     elif randomNumber == 4:
-        button(DISPLAY, 540, 230, 100, 100, PURPLE, selectCurrent)
+        pygame.draw.rect(DISPLAY, BLUE, (540, 230, 100, 100))
     elif randomNumber == 5:
-        button(DISPLAY, 540, 230, 100, 100, YELLOW, selectCurrent)
+        pygame.draw.rect(DISPLAY, PURPLE, (540, 230, 100, 100))
+    currentColor = randomNumber
 
-def calculateTime(start, end):
+def displayAllStops():
+    button(15, 25, 100, 100, RED, selectRed)
+    button(15 + (110 * 1), 25, 100, 100, BROWN, selectBrown)
+    button(15 + (110 * 2), 25, 100, 100, GREEN, selectGreen)
+    button(15 + (110 * 3), 25, 100, 100, YELLOW, selectYellow)
+    button(15 + (110 * 4), 25, 100, 100, BLUE, selectBlue)
+    button(15 + (110 * 5), 25, 100, 100, PURPLE, selectPurple)
+
+def displayExpected(init):
+    if init == True:
+        text("Expected Arrival:", 100, 500)
+    else:
+        erase(174, 245, 100, 22)
+        text("Expected Arrival: {:0.2f}".format(calculateExpected()), 100, 500)
+
+def displayActual(init):
+    if init == True:
+        text("Actual Arrival: ", 100, 550)
+    else:
+        erase(154, 270, 100, 22)
+        text("Actual Arrival: {:0.2f}".format(calculateActual()), 100, 550)
+        displayDelay(False)
+
+def displayDelay(init):
+    if init == True:
+        text("Delay:", 100, 600)
+    else:
+        erase(97, 295, 230, 22)
+        text("Delay: -{:0.2f}".format(calculateDelay()), 100, 600)
+
+def displayAllComponents():
+    displayAllStops()
+    displayExpected(True)
+    displayActual(True)
+    displayDelay(True)
+
+def displayArriveMessage():
+    text("You are arriving shortly!", 500, 650)
+
+# Handles selecting a new bus route every 10 seconds
+def calculateTotalTime():
     return end - start
 
+def calculateExpected():
+    return end - expected
+
+def calculateActual():
+    return end - actual
+
+# Calculates the delay from the bus route displaying to the user
+# selecting the correct route
 def calculateDelay():
-    #Actual - Expected
-    return "00:10"
+    return end - expectedEnd
 
 def main():
-    pygame.init()
-    size = (width, height) = (685, 400)
-    DISPLAY = pygame.display.set_mode(size)
     pygame.display.set_caption('Bus Stop Manager')
+    DISPLAY.fill(WHITE)
 
     global start
     global end
+    global expected
+    global actual
+    global currentColor
+    global expectedEnd
 
     start = time.time()
+    expected = time.time()
+    actual = time.time()
+    currentColor = -1
+    iterations = 0
 
-    #clock = pygame.time.Clock()
+    run = True
 
-    DISPLAY.fill(WHITE)
-
-    while True:
+    while run:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-
-        #print("Start: {}".format(start))
         end = time.time()
-        if(calculateTime(start, end) >= 10):
-            #print(calculateTime(start, end))
-            print("Display current")
+        elapsedTime = calculateTotalTime()
+        if(elapsedTime >= 5 and elapsedTime < 10):
+            displayArriveMessage()
+        elif(elapsedTime >= 10):
             start = time.time()
-            showCurrent(DISPLAY)
+            erase(240, 320, 200, 30)
+            actual = time.time() - calculateExpected()
+            expectedEnd = time.time()
+            displayExpected(False)
+            displayCurrent()
+            iterations += 1
                     
-        button(DISPLAY, 15, 25, 100, 100, RED, selectRed)
-        button(DISPLAY, 15 + (110 * 1), 25, 100, 100, BROWN, selectBrown)
-        button(DISPLAY, 15 + (110 * 2), 25, 100, 100, GREEN, selectGreen)
-        button(DISPLAY, 15 + (110 * 3), 25, 100, 100, YELLOW, selectYellow)
-        button(DISPLAY, 15 + (110 * 4), 25, 100, 100, BLUE, selectBlue)
-        button(DISPLAY, 15 + (110 * 5), 25, 100, 100, PURPLE, selectPurple)
-
-        text(DISPLAY, "Expected Arrival: ", 100, 500)
-        text(DISPLAY, "Actual Arrival: ", 100, 550)
-        text(DISPLAY, "Delay: "+ calculateDelay(), 100, 600)
-
-        text(DISPLAY, "Current Stop:", 1085, 410)
+        displayAllComponents()
+        
+        text("Current Stop", 1085, 410)
         
         pygame.display.update()
-        #clock.tick(15)
+        
+        #Code to stop after a certain amount of iterations
+        if iterations == MAX_AMOUNT:
+            text("You've completed your routes! Thank you!", 400, 650)
+            erase(540, 230, 100, 100)
+            run = False
 
 if __name__ == "__main__":
     main()
